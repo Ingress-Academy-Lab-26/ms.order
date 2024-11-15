87ca6c032f5c5fb45057927c5ff21735a8fe9491
KenanOrucov <kenannorucon19@gmail.com>
2024-11-15 18:59:55 +0400

    deleting comments and empty lines

--- src/main/java/az/ingress/ms_order/config/JacksonConfiguration.java
+++ src/main/java/az/ingress/ms_order/config/JacksonConfiguration.java
@@ -9,15 +9,6 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 public class JacksonConfiguration {
 
-//    @Bean
-//    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
-//        return builder -> {
-//            builder.simpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//            builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-//            builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-//        };
-//    }
-
     @Bean
     @Primary
     public ObjectMapper objectMapper() {
